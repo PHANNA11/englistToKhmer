b5d9a364841edb8b0f4015cf4c64bcda54cab5a3
PHANNA11 <phaphanna2018@gmail.com>
2023-05-09 19:56:09 +0700

    switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
 
@@ -9,19 +11,20 @@ void main() async {
   await switchLanguageController.initLanguage();
   runApp(MyApp());
 }
+
 SwitchLanguageController switchLanguageController =
     Get.put(SwitchLanguageController());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-       translations: ChangeLocalControlller(),
-          locale: switchLanguageController.english.value
-
-              ? const Locale('en', 'US')
-              : const Locale('KH', 'KH'),
+    return GetMaterialApp(
+      translations: ChangeLocalControlller(),
+      locale: switchLanguageController.english.value
+          ? const Locale('en', 'US')
+          : const Locale('KH', 'KH'),
       home: HomePage(),
     );
   }
@@ -35,12 +38,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  SwitchLanguageController controlller = Get.put(SwitchLanguageController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('hello'.tr),
       ),
+      body: GetBuilder<SwitchLanguageController>(
+          init: controlller,
+          builder: (context) {
+            return Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('ខ្មែរ '),
+                  CupertinoSwitch(
+                    value: controlller.english.value,
+                    onChanged: (value) {
+                      controlller.switchLanguage(value);
+                      var localeEng = const Locale('en', 'US');
+                      var localeKh = const Locale('KH', 'KH');
+                      Get.updateLocale(value ? localeEng : localeKh);
+                    },
+                  ),
+                  const Text(' English')
+                ],
+              ),
+            );
+          }),
     );
   }
 }
